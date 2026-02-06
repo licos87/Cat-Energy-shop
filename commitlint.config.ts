8bdeb119bcb044bf09df929c5d94c83eadf83e83
Sergey Ivanov <licos.87@mail.ru>
2026-02-06 23:32:56 +0300

    chore: удалить закоментированную строку

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,2 +1,37 @@
+import { type UserConfig } from '@commitlint/types';
 
-export default { extends: ['@commitlint/config-conventional'] };
+
+
+
+
+export default {
+	// Use the conventional commit rules as a base.
+	extends: ['@commitlint/config-conventional'],
+	prompt: {
+		questions: {
+			type: {
+				enum: {
+					// Add a space to a few common types for better alignment.
+					build: {
+						emoji: '🛠️ ', // The extra space fixes the alignment.
+					},
+					chore: {
+						emoji: '♻️ ',
+					},
+					ci: {
+						emoji: '⚙️ ',
+					},
+					revert: {
+						emoji: '🗑️ ',
+					},
+					feat: {
+						emoji: '🧩️ ',
+					},
+					bugfix: {
+						emoji: '💊️ ',
+					},
+				},
+			},
+		},
+	},
+} satisfies UserConfig;
